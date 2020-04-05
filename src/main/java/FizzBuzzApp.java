4df7db6bcf1d5ca33a8e44f1ff0c22b2ca2b266c
andreea <andreeaboargm@gmail.com>
2020-04-05 19:45:07 +0300

    step 1

--- src/main/java/FizzBuzzApp.java
+++ src/main/java/FizzBuzzApp.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FizzBuzzApp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        FizzBuzzUtil.printFizzBuzz(n);
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,4 +1,36 @@
-package test.java;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FizzBuzzTest {
+    @Test
+    public void doNotConvertIfNormal() {
+        Assert.assertEquals("1", FizzBuzzUtil.convert(1));
+        Assert.assertEquals("2", FizzBuzzUtil.convert(2));
+    }
+
+    @Test
+    public void convertToFizzIfMultipleOf3() {
+        Assert.assertEquals("fizz", FizzBuzzUtil.convert(3));
+    }
+
+    @Test
+    public void convertToBuzzIfMultipleOf5() {
+        Assert.assertEquals("buzz", FizzBuzzUtil.convert(5));
+    }
+    @Test
+    public void convertToFizzBuzzIfMultipleOf3And5() {
+        Assert.assertEquals("fizzbuzz", FizzBuzzUtil.convert(15));
+    }
+    @Test
+    public void createFizzBuzzListFrom1ToGivenN() {
+        int n = 20;
+        List<String> expectedFizzBuzzList = new ArrayList<>(List.of("1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz",
+                "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16", "17", "fizz", "19", "buzz"));
+        Assert.assertEquals(expectedFizzBuzzList, FizzBuzzUtil.createFizzBuzzList(n));
+    }
+
 }
